d098e2a1131e57f180f2104827ac0fa98a8d4957
julio-silva93 <juliocraftwar@gmail.com>
2024-06-12 22:07:19 -0300

    implementado sistema de localizacao

--- views/public/login.js
+++ views/public/login.js
@@ -1,7 +1,58 @@
 const login = document.getElementById('loginForm');
 const cadastro = document.getElementById('cadastroForm');
-Cookies.set = ('teste','1');
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+const aplicacao = document.getElementById('aplicacao');
+const localizarErro = document.getElementById('localErro');
+let user = Object;
+
+
+//função para definir cookies.
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue;
+}
+
+//função para receber o valor de cookies.
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+//função utilizada para receber a geolocalização;
+function localizarUsuario() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(guardarLocalizacao);
+    } else {
+      localErro();
+      document.getElementById('erroDescricao').innerHTML = "Infelizmente seu dispositivo ou navegador não suporta geolocalização.";
+      document.getElementById('erroBotao').style.display = "none";
+    }
+}
+
+//função utilizada para armazenar a geolocalização em uma variável.
+function guardarLocalizacao(pos) {
+    user.lat = pos.coords.latitude;
+    user.lon = pos.coords.longitude;
+}
+
+function checarLocal() {
+    if(typeof user.lat == 'undefined'){
+        localErro();
+        
+    } else {
+        mudarLogin();
+    }
+}
+
+document.getElementById('loginForm').addEventListener('submit', function (event) {
     event.preventDefault();
     const email = document.getElementById('emailLogin').value;
     const password = document.getElementById('passwordLogin').value;
@@ -9,7 +60,8 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     firebase.auth().signInWithEmailAndPassword(email, password)
         .then((userCredential) => {
             // Login bem-sucedido
-            console.log(userCredential)
+            user = userCredential;
+            liberarEntrada();
             //window.location.href = 'index.html'; // Redireciona para a página de cadastro de alunos
         })
         .catch((error) => {
@@ -17,7 +69,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
             alert('Falha ao entrar: ' + error.message);
         });
 });
-document.getElementById('cadastroForm').addEventListener('submit', function(event) {
+document.getElementById('cadastroForm').addEventListener('submit', function (event) {
     event.preventDefault();
     const email = document.getElementById('emailCadastro').value;
     const password = document.getElementById('passwordCadastro').value;
@@ -26,21 +78,41 @@ document.getElementById('cadastroForm').addEventListener('submit', function(even
         .then((userCredential) => {
             // Login bem-sucedido
             console.log('userCredential')
-            window.location.href = 'index.html'; // Redireciona para a página de cadastro de alunos
+            
         })
         .catch((error) => {
             console.error('Erro ao fazer cadastro: ', error.message);
             alert('Falha ao cadastrar: ' + error.message);
         });
 });
-function mudarCadastro(){
-    login.style.display = "none"
-    cadastro.style.display = "flex"
-    titulo.innerHTML = "Cadastro"
+
+//função para mudar para a tela de cadastro
+function mudarCadastro() {
+    login.style.display = "none";
+    cadastro.style.display = "flex";
 }
 
-function mudarLogin(){
-    login.style.display = "flex"
-    cadastro.style.display = "none"
-    titulo.innerHTML = "Login"
+//função para mudar para a tela de login
+function mudarLogin() {
+    login.style.display = "flex";
+    cadastro.style.display = "none";
+    localizarErro.style.display = "none";
 }
+
+//função para liberar a entrada
+function liberarEntrada() {
+    login.style.display = "none";
+    cadastro.style.display = "none";
+    aplicacao.style.display = "block";
+}
+
+//função para bloquear a aplicação caso a localização não esteja habilitada.
+function localErro() {
+    login.style.display = "none";
+    cadastro.style.display = "none";
+    aplicacao.style.display = "none";
+    localizarErro.style.display = "block";
+}
+
+localizarUsuario();
+setTimeout(checarLocal,100);
--- views/public/script.js
+++
@@ -1,4 +0,0 @@
-function limparConsulta() {
-    document.getElementById('nomeConsulta').value = '';  // Limpa o campo de consulta
-    document.getElementById('listaAlunos').innerHTML = '';  // Limpa a lista de resultados
-}
